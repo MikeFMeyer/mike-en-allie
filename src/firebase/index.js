675f7c5fa7e52d5c7ccf4c865c1516ad07841cf4
Mike Meyer <mfmeyer301@gmail.com>
2022-10-22 00:35:21 +0200

    new config

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,13 +1,17 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { getFirestore } from "firebase/firestore";\
+
 const firebaseConfig = {
+
   apiKey: import.meta.env.FIREBASE_API_KEY,
-  authDomain: "wedding-invites-5f1fe.firebaseapp.com",
-  projectId: "wedding-invites-5f1fe",
-  storageBucket: "wedding-invites-5f1fe.appspot.com",
-  messagingSenderId: "681510411650",
-  appId: "1:681510411650:web:3353726b50d0d4b420c39b"
+  authDomain: "mike-en-allie-invites.firebaseapp.com",
+  projectId: "mike-en-allie-invites",
+  storageBucket: "mike-en-allie-invites.appspot.com",
+  messagingSenderId: "1052926152786",
+  appId: "1:1052926152786:web:fb611ace36f72a05908f8b"
+
 };
+
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
