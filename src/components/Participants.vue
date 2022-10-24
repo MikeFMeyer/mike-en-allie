8ededa96e9e75f76d50acd0aa4263b45663f13d1
Mike Meyer <mfmeyer301@gmail.com>
2022-10-25 00:50:22 +0200

    center modal

--- src/components/Participants.vue
+++ src/components/Participants.vue
@@ -68,7 +68,7 @@
 
 			<div class="fixed inset-0 z-10 overflow-y-auto">
 				<div
-					class="flex items-end justify-center min-h-full p-4 text-center sm:items-center sm:p-0"
+					class="flex justify-center min-h-full p-4 text-center items-center sm:p-0"
 				>
 					<TransitionChild
 						as="template"
