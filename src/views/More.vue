52df15812b7f9f53f4488d5fe4fc6168a31cad50
Mike Meyer <mfmeyer301@gmail.com>
2022-10-29 02:17:56 +0200

    places to stay

--- src/views/More.vue
+++ src/views/More.vue
@@ -4,17 +4,32 @@
       <div class="mx-auto max-w-3xl divide-y-2 divide-gray-200">
         <h2 class="text-center text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">More Wedding Related Info</h2>
         <dl class="mt-10 space-y-6 divide-y divide-gray-200">
-          <Disclosure as="div" v-for="faq in faqs" :key="faq.question" class="pt-6" v-slot="{ open }">
+          <Disclosure as="div" class="pt-6" v-slot="{ open }" defaultOpen="true">
             <dt class="text-lg">
               <DisclosureButton class="flex w-full items-start justify-between text-left text-gray-400">
-                <span class="font-medium text-gray-900">{{ faq.question }}</span>
+                <span class="font-medium text-gray-900">Places to stay in the area</span>
                 <span class="ml-6 flex h-7 items-center">
                   <ChevronDownIcon :class="[open ? '-rotate-180' : 'rotate-0', 'h-6 w-6 transform']" aria-hidden="true" />
                 </span>
               </DisclosureButton>
             </dt>
-            <DisclosurePanel as="dd" class="mt-2 pr-12">
-              <p class="text-base text-gray-500">{{ faq.answer }}</p>
+            <DisclosurePanel as="div" class="mt-10">
+							<ul role="list">
+								<li v-for="location in locations" :key="location.id" class="relative py-5 px-4 border-b border-solid border-gray-400 last:border-b-0">
+									<a :href="location.link" target="_blank">
+										<div class="flex justify-between">
+											<div class="min-w-0 flex-1">
+												<span class="absolute inset-0" aria-hidden="true" />
+												<p class="text-md font-medium text-gray-900">{{ location.name }}</p>
+											</div>
+											<span class="flex-shrink-0 whitespace-nowrap text-sm text-gray-500">{{ location.distance }} from venue</span>
+										</div>
+										<div class="mt-4">
+											<p class="text-sm text-gray-600 line-clamp-2">{{ location.link }}</p>
+										</div>
+									</a>
+								</li>
+							</ul>
             </DisclosurePanel>
           </Disclosure>
         </dl>
@@ -27,21 +42,42 @@
 import { Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/vue'
 import { ChevronDownIcon } from '@heroicons/vue/24/outline'
 
-const faqs = [
-
-
+const locations = [
   {
-    question: "What's the best thing about Switzerland?",
-    answer:
-      "I don't know, but the flag is a big plus.",
+    id: 1,
+    name: 'Klyne Jiweel Guest House',
+    link: 'https://goo.gl/maps/vinrgdTSZBzCpNwCA',
+    distance: '600m',
   },
   {
-		question: "How do you follow Will Smith in the snow?",
-		answer: "You follow the fresh prints."
-	},
+    id: 2,
+    name: 'Batter Boys',
+    link: 'https://goo.gl/maps/hXpEt6VPobDBg7cw9',
+    distance: '1.1km',
+  },
   {
-		question: "Have you heard about the chocolate record player?",
-		answer: "It sounds pretty sweet."
-	},
+    id: 3,
+    name: 'Prinshof Manor',
+    link: 'https://g.page/prinshofmanorgh?share',
+    distance: '3.8km',
+  },
+  {
+    id: 4,
+    name: 'Delectus Manor Guest Lodge',
+    link: 'https://goo.gl/maps/yiMqgxP8V11ns6wVA',
+    distance: '1.6km',
+  },
+  {
+    id: 5,
+    name: 'Sheraton Pretoria Hotel',
+    link: 'https://goo.gl/maps/nmqgpDQGu8Fz7WccA',
+    distance: '16km',
+  },
+  {
+    id: 6,
+    name: 'Villa San Giovanni Accommodation',
+    link: 'https://goo.gl/maps/KTvhru4Ukndnkgmr6',
+    distance: '6.5km',
+  },
 ]
 </script>
